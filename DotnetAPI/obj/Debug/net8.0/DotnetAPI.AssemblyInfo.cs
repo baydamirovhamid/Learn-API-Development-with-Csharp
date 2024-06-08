a05d345d290c07af5735516cfb6f37813cf1a23f
Hamid Baydamirov <baydamirovv35@gmail.com>
2024-06-09 03:04:22 +0400

    addPostModel

--- DotnetAPI/obj/Debug/net8.0/DotnetAPI.AssemblyInfo.cs
+++ DotnetAPI/obj/Debug/net8.0/DotnetAPI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DotnetAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+50c863a3c7034ae059e4e803b96d16098b882ae5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e4a7bb47b8c8589304661f27daca99ea8b6b361d")]
 [assembly: System.Reflection.AssemblyProductAttribute("DotnetAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DotnetAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
