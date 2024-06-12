dcaadb575deff5c63e9a7e7eb4c3aa76e9b5fca7
Hamid Baydamirov <baydamirovv35@gmail.com>
2024-06-12 23:52:29 +0400

    addDatabasefolder

--- DotnetAPI/obj/Debug/net8.0/DotnetAPI.AssemblyInfo.cs
+++ DotnetAPI/obj/Debug/net8.0/DotnetAPI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DotnetAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a05d345d290c07af5735516cfb6f37813cf1a23f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+de043809ad46b38f680aec1712d557b58f84e44f")]
 [assembly: System.Reflection.AssemblyProductAttribute("DotnetAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DotnetAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
